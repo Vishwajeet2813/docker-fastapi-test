39dcf38566726f745dab34ad3d407b9ff3175fef
Vishwajeet2813 <vishwajeetlad9834@gmail.com>
2026-04-17 15:22:30 +0530

    changes some files

--- app/main.py
+++ app/main.py
@@ -4,22 +4,19 @@ from fastapi import FastAPI
 from app import services
 from app.schema import UserIn, BaseResponse, UserListOut
 
+# ✅ ADD THIS IMPORT
+from prometheus_fastapi_instrumentator import Instrumentator
+
 app = FastAPI()
 
 
 @app.get("/")
 async def index():
-    """
-    Index route for our application
-    """
     return {"message": "Hello from FastAPI ;)"}
 
 
 @app.post("/users", response_model=BaseResponse)
 async def user_create(user: UserIn):
-    """
-    Add user data to json file
-    """
     try:
         services.add_userdata(user.dict())
     except:
@@ -29,7 +26,7 @@ async def user_create(user: UserIn):
 
 @app.get("/users", response_model=UserListOut)
 async def get_users():
-    """
-    Read user data from json file
-    """
     return services.read_usersdata()
+
+
+Instrumentator().instrument(app).expose(app)
